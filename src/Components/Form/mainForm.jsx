738b32fad050c8a6a2d5de6646bf64330d86325f
mohan13 <mohangrg250@gmail.com>
2022-06-11 05:05:00 -0700

    with tailwind

--- src/Components/Form/mainForm.jsx
+++ src/Components/Form/mainForm.jsx
@@ -16,12 +16,15 @@ const MainForm = () => {
       validationSchema={schema}
       render = {({errors,touched,handleSubmit})=>{
         return (
-          <Form onSubmit={handleSubmit}>
+          <Form onSubmit={handleSubmit} className="grid grid-col-1 gap-10  ">
             <label>Username</label>
-            <Field type='text' name='username'/>
-            <ErrorMessage name='username' component={'div'}/>
-            <Field type='password' name='password'/>
-            <ErrorMessage name='password' component={'div'}/>
+            <Field className=" w-40 border mx-auto" type='text' name='username'/>
+            <div className='relative w-full h-screen intent-40 grid place-content-center top-0 left-0 bg-green-300'>
+            <ErrorMessage className='absolute z-20 top-0 left-0  grid place-content-center bg-red-200 ' name='username' component={'div'}/>
+            </div>
+            {/* <label>Password</label>
+            <Field className="w-40 border mx-auto" type='password' name='password'/>
+            <ErrorMessage name='password' component={'div'}/> */}
             <button type='submit'>Submit</button>
           </Form>
         )
